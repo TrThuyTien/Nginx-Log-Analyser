365ed14c63d3874f82838a5c51855550f45b930c
trthuytien <tranthithuytien0615@gmail.com>
2025-07-08 17:02:55 +0700

    update code

--- nginx_log_analyser.sh
+++ nginx_log_analyser.sh
@@ -1,62 +1,43 @@
 #!/bin/bash
 
 # Filter IP, request paths, response status codes, user agents
+if [ ! -d "tmp/" ]; then 
+    mkdir tmp
+fi
+
+if [ -d tmp ]; then
+    rm -rf tmp/*
+fi
+
 awk -F '"' '{ 
     split($1, a, " "); 
     split($2, b, " "); 
     split($3, c, " "); 
-    print a[1]"-,-"b[2]"-,-"c[1]"-,-"$(NF-1)
-}' nginx-access.log.txt > filter_request.txt
+    print a[1]"|"b[2]"|"c[1]"|"$(NF-1)"|"
+}' nginx-access.log.txt |
+while IFS='|' read -r ip path code ua; do
+    echo "$ip" >> tmp/ip_list.txt;
+    echo "$path" >> tmp/path_list.txt;
+    echo "$code" >> tmp/code_list.txt;
+    echo "$ua" >> tmp/ua_list.txt;
+done
 
-# Function to count and display the top 5 most frequent values for a given field
-function most_requests() {
-    local field_index=$1 # Field position
-    local field_name=$2  # Field name
-    local output_file="${field_name}.txt"
-
-    # Get all values for the field sort them for counting
-    local all_requests=$(awk -F'-,-' -v idx="$field_index" '{ print $idx }' filter_request.txt | sort)
-    
-    # Initialize counting variable
-    local pre_request=""
-    local count_request=0
-
-    # Create or clear output file
-    > "$output_file" 
-
-    # Count requests
-    echo "$all_requests" | while read -r line; do
-        local curr_request=$(echo "$line" | tr -d '\n')
-        if [ "$pre_request" = "$curr_request" ]; then
-            ((count_request++))
-        else 
-            if [ -n "$pre_request" ]; then
-                echo "$pre_request - $count_request requests" >> "$output_file"
-            fi
-            count_request=1
-        fi
-        pre_request=$curr_request
-    done
-    if [ -n "$pre_request" ]; then
-        echo "$pre_request - $count_request requests" >> "$output_file"
-    fi
-
-    # Sort and print 5 most ip request
-    sort -rt " " -n -k3,3 "$output_file" | awk 'NR<=5'
+function top_5() {
+    local inputfile=$1
+    local title_output=$2
+    # Top 5
+    output=$(sort -n "$inputfile" | uniq -c | sort -nr -k1,1 | head -n 5)
+    echo "$title_output: "
+    echo "$output" | awk '{
+        count=$1;
+        $1="";
+        sub(/^ /, "");
+        print $0" - "count" requests"
+    }'
+    echo ""
 }
 
-echo "Top 5 IP addresses with the most requests: "
-most_requests 1 "ip_addresses"
-echo ""
-
-echo "Top 5 most requested paths: "
-most_requests 2 "requested_paths"
-echo ""
-
-echo "Top 5 response status codes: "
-most_requests 3 "status_codes"
-echo ""
-
-echo "Top 5 user agents: "
-most_requests 4 "user_agents"
-echo ""
+top_5 tmp/ip_list.txt "Top 5 IP addresses"
+top_5 tmp/path_list.txt "Top 5 request paths"
+top_5 tmp/code_list.txt "Top 5 response status codes"
+top_5 tmp/ua_list.txt "Top 5 user agents"
